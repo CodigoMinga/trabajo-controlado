8c099810212c0ced2453d9edb0b1514d5e2813c8
Sebastian <sebastian.vera.moll@gmail.com>
2021-05-20 13:27:54 -0400

    se crea form , rutas, funciones en controller de proyect, se relaciona proyect con item,

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Rutas CLientes
 Route::get('/clients/list','ClientController@list')->name('clients.list');
 Route::get('/clients/add','ClientController@add')->name('clients.add');
 Route::post('/clients/add/process','ClientController@addProcess');
@@ -23,4 +25,11 @@ Route::get('/clients/details','ClientController@details')->name('clients.details
 Route::get('/clients/{client_id}','ClientController@details')->name('clients.details');
 Route::post('/clients/{client_id}/edit/process','ClientController@editprocess');
 Route::get('/clients/{client_id}/delete','ClientController@delete');
+
+//Rutas Proyectos
+route::get('/proyects/list','ProyectController@list')->name('proyects.list');
+route::get('/proyects/add','ProyectController@add')->name('proyects.add');
+route::get('/proyects/{proyect_id}','ProyectController@details')->name('proyects.details');
+route::get('/proyects/{proyect_id}/delete','ProyectController@delete')->name('proyects.delete');
+route::post('/proyects/process', 'ProyectController@process');
 
